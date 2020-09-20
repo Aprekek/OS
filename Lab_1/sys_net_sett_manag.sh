5560711c39489d5b3a2a01686cd5dfe0cf34e248
Aprekek <mhlshamburov@gmail.com>
2020-09-20 22:02:51 +0700

    Initial commit

---
+++ Lab_1/sys_net_sett_manag.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+function networkInterfaces(){
+    total=0
+    echo -e "\nСетевые интерфейсы: "
+    for i in $(ifconfig -a)
+    do
+        if [ ${i:${#i}-1} == ":" ] 
+        then
+            if [ $total != 0 ]
+            then
+                printf "\n\n$total) $i "
+            else
+                printf "$total) $i "
+            fi
+            let total+=1
+            findNICState $i
+        else
+            case $i in
+            "RX" | "TX" | "inet" | "inet6" | "ether" | "loop" | "device" | "flags"*)
+                printf "\n\t"$i" "
+                ;;
+            *)
+                printf $i" "
+                ;;
+            esac
+        fi
+    done
+    printf "\n"
+}
+
+function findNICState(){
+    isNextWordState=0
+    for word in $(ip a | grep "$1")
+    do
+        if [ $word == "state" ]
+        then
+            printf "$word "
+            let isNextWordState=1
+        else 
+            if [ $isNextWordState == 1 ]
+            then
+                printf $word
+                break
+            fi
+        fi
+    done
+}
+
+function switchOffNICViaIP(){
+    NIC="$(ifconfig -a | grep -B3 "192.168.0.59" | grep ":" | cut -f 1 -d ':')"
+    printf "$(ip link set $NIC down)"
+}
+
+function about(){
+   echo "Авторы (ИС-841): Шамбуров Михаил"
+   echo "Аргументы:"
+   echo -e "\t2)--nic|-n - вывести информацию о сетевых интерфейсах"
+   echo -e "\t  Пример: ./sys_net_sett_manag -n"
+   echo -e "\t1)<NIC> [up|down] - включить/отключить сетевой(ые) интерфейс(ы)"
+   echo -e "\t  Пример: ./sys_net_sett_manag wlp5s0 up ; или, если несколько ./sys_net_sett_manag wlp5s0 up enp3s0 down"
+   echo -e "\t3)-ip <NIC> <x.x.x.x> - установка IP адресса для сетевого интерфейса"
+   echo -e "\t  Пример: ./sys_net_sett_manag -ip wlp5s0 192.169.0.202"
+   echo -e "\t4)-m <NIC> <x.x.x.x> - установка netmask для сетевого интерфейса"
+   echo -e "\t  Пример: ./sys_net_sett_manag -m wlp5s0 255.255.254.0"
+   echo -e "\t5)-dl <IP> - отключение сетевого интерфейса по IP"
+   echo -e "\t  Пример: ./sys_net_sett_manag -dl 192.168.0.59"
+   echo -e "\t6)-kill <port ID> - убийство процесса по занимаемому порту"
+   echo -e "\t  Пример: ./sys_net_sett_manag -kill 22"
+}
+
+#------------------------MAIN-----------------------------
+
+if [ $# -lt 1 ]
+then
+    echo "Обязателен хотябы 1 параметр воспользуйтесь --help|-h"
+    exit
+fi
+
+while [ -n "$1" ]
+do
+    case $1 in
+    "-h" | "--help")
+        about
+        ;;
+    "up" | "down")
+        NIC=$param # Имя сетевого интерфейса
+        if [ -n "$(ifconfig -a | grep "$NIC: ")" ]
+        then
+            printf "$(ifconfig $NIC $1)"
+        else
+            echo -en "\033[31m \nСетевого интерфейса $NIC не существует \033[0m"
+        fi
+        ;;
+    "-n" | "--nic")
+        networkInterfaces
+        ;;
+    "-ip")
+        shift
+        NIC=$1 # Имя сетевого интерфейса
+        shift # Переходим к IP дрессу
+        printf "$(ifconfig $NIC $1)"
+        ;;
+    "-m")
+        shift
+        NIC=$1 # Имя сетевого интерфейса
+        shift # Переходим к netmask
+        printf "$(ifconfig $NIC netmask $1)"
+        ;;
+    "-dl")
+        shift # переход к IP адрессу порта
+        switchOffNICViaIP $1
+        ;;
+    "-kill")
+        shift # переходим к номеру порта
+        printf "$(kill -9 $(sudo lsof -t -i:$1))"
+        ;;
+    *)
+        echo "Неверная команда \"$1\""
+    esac
+
+    param="$1"
+    shift
+done
